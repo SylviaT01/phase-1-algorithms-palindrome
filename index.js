2b1e5bc045e1d3df6ccda4dbd1e3bb8bfe46d7e2
Sylvia <sylviachebet03@gmail.com>
2024-04-04 14:40:04 +0300

    add

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  return string === string.split('').reverse().join('')
 }
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("robot"));
+
 
 /* 
   Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+  str.split(''): This part of the code splits the input string str into an array of characters.
+  reverse(): This method reverses the order of the elements in the array obtained from splitting the string
+  .join(''): This method joins all the elements of the reversed array back into a single string. 
+  
 */
 
 // You can run `node index.js` to view these console logs
